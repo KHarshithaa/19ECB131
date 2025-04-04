cc780b1ee083dfa47258d97e0a4d3d4d7919d51f
KHarshithaa <hkoduru@student.gitam.edu>
2025-04-04 10:34:55 +0530

    Create bubble sort.c

---
+++ bubble sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Bubble Sort function
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Display array
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    display(arr, n);
+
+    bubble_sort(arr, n);
+
+    printf("Sorted array (Bubble Sort):\n");
+    display(arr, n);
+
+    return 0;
+}
